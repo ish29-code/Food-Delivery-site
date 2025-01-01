0f7f1c8014b83d354122445b90bf534b29d7541d
Ishika Deshpande <ishikadeshpande816@gmail.com>
2025-01-01 12:27:32 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Food-Delivery-Site/',
+ base: '/Food-Delivery-Site/',
   plugins: [react()],
-  server: { port: 4000 },
-  build: { outDir: '.' // This ensures the build output goes to the root directory 
+  server: { port: 5173 },
+  build: { outDir: 'docs' // This ensures the build output goes to the root directory 
   }
 })
