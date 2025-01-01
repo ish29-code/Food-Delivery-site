e981161d891d84dff8d27b8d025e4644576da1b9
Ishika Deshpande <ishikadeshpande816@gmail.com>
2025-01-01 23:50:47 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -7,8 +7,22 @@ export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
   build: { outDir: 'docs' // This ensures the build output goes to the root directory 
-           rollupOptions: { 
-              external: ['/Food-Delivery-Site/assets/index-UirRUTxs.js'], // Add external modules here 
-     },
+  }
+})*/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Food-Delivery-Site/',
+  plugins: [react()],
+  server: { port: 5173 },
+  build: {
+    outDir: 'docs', // This ensures the build output goes to the 'docs' directory
+    rollupOptions: {
+      external: ['/Food-Delivery-Site/assets/index-UirRUTxs.js'], // Add external modules here
+    },
   },
 });
+
